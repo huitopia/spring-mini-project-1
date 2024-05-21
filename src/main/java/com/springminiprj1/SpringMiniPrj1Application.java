e8603d304d90bcc5e74f3458f9a91083004c356d
huitopia <huitopia27@gmail.com>
2024-05-21 10:13:57 +0900

    🎉 : first commit

---
+++ src/main/java/com/springminiprj1/SpringMiniPrj1Application.java
@@ -0,0 +1,13 @@
+package com.springminiprj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMiniPrj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMiniPrj1Application.class, args);
+    }
+
+}
