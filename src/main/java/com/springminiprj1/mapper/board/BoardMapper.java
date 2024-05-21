d9b6d0f96c6ce94b38ac015b91baede617ac008e
huitopia <huitopia27@gmail.com>
2024-05-21 14:29:33 +0900

    ✨ : Select Board List

--- src/main/java/com/springminiprj1/mapper/board/BoardMapper.java
+++ src/main/java/com/springminiprj1/mapper/board/BoardMapper.java
@@ -3,6 +3,9 @@ package com.springminiprj1.mapper.board;
 import com.springminiprj1.domain.board.Board;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
@@ -12,4 +15,11 @@ public interface BoardMapper {
             VALUES (#{title}, #{content}, #{writer})
             """)
     int add(Board board);
+
+    @Select("""
+            SELECT id, title, writer
+            FROM board
+            ORDER BY id DESC
+            """)
+    List<Board> selectAll();
 }
