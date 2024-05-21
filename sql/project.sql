50c45d518c0b2a20b09e5d70e21b3a70d8c6f6a1
huitopia <huitopia27@gmail.com>
2024-05-21 12:12:13 +0900

    ✏️ : 오타 수정

--- sql/project.sql
+++ sql/project.sql
@@ -3,9 +3,9 @@ USE prj01;
 # Board Table
 CREATE TABLE board
 (
-    id      INT PRIMARY KEY AUTO_INCREMENT,
-    title   VARCHAR(100)  NOT NULL,
-    content VARCHAR(1000) NOT NULL,
-    writer  VARCHAR(100)  NOT NULL,
-    inseted DATETIME      NOT NULL DEFAULT NOW()
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(100)  NOT NULL,
+    content  VARCHAR(1000) NOT NULL,
+    writer   VARCHAR(100)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
 );
