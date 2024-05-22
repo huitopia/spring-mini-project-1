6c0ae6596ff17ed1658b3214e99af447bb59e3b3
huitopia <huitopia27@gmail.com>
2024-05-23 01:16:04 +0900

    ✨ : Member.java

---
+++ src/main/java/com/springminiprj1/domain/member/Member.java
@@ -0,0 +1,22 @@
+package com.springminiprj1.domain.member;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+public class Member {
+    private Integer id;
+    private String email;
+    private String password;
+    private String nickName;
+    private LocalDateTime inserted;
+
+    public String getSignupDateAndTime() {
+        DateTimeFormatter formatter
+                = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초");
+
+        return inserted.format(formatter);
+    }
+}
