99c8493a16ff64cba61f33210fcb653b5d3b2267
huitopia <huitopia27@gmail.com>
2024-05-23 01:20:59 +0900

    ✨ : MemberController.java

---
+++ src/main/java/com/springminiprj1/controller/member/MemberController.java
@@ -0,0 +1,71 @@
+package com.springminiprj1.controller.member;
+
+import com.springminiprj1.domain.member.Member;
+import com.springminiprj1.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+
+    final MemberService service;
+
+    @PostMapping("signup")
+    public ResponseEntity signup(@RequestBody Member member) {
+        if (service.validate(member)) {
+            service.add(member);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping(value = "check", params = "email")
+    public ResponseEntity checkEmail(@RequestParam("email") String email) {
+        Member member = service.getByEmail(email);
+        if (member == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(email);
+    }
+
+    @GetMapping(value = "check", params = "nickName")
+    public ResponseEntity checkNickName(@RequestParam("nickName") String nickName) {
+        Member member = service.getByNickName(nickName);
+        if (member == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(nickName);
+    }
+
+    @GetMapping("list")
+    public List<Member> list() {
+        return service.list();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity get(@PathVariable Integer id) {
+        Member member = service.getById(id);
+        if (member == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(member);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@RequestBody Member member) {
+        if (service.hasAccess(member)) {
+            service.remove(member.getId());
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
